be5ebe0edf96c9167df54aef3630c5c89f66c3ff
hhon <alexhnhon@gmail.com>
2022-04-03 01:40:37 -0700

    bareform

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import ProductCard from "../components/productCard";
 import Layout from "../components/layout";
 import Mailinglist from "../components/mailinglist";
 import * as styles from "../style/home.module.css";
+import SubscriptionForm from '../components/sub-form'
 
 const IndexPage = () => {
   return (
     <Layout>
       <Mailinglist />
+      <SubscriptionForm/>
       <p className={styles.banner}>Picks of the season!</p>
       <ProductCard />
     </Layout>
